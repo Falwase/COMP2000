61a13babddea26cb84ad92f79bf7e2fe4604ed7e
Falwase <rishi.gogada@gmail.com>
2023-09-24 23:37:11 +0930

    reardasfs

--- prac5/Mult.asm
+++ prac5/Mult.asm
@@ -7,3 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@2
+M=0
+
+@0
+D=M
+@3
+M=D
+
+@1
+D=M
+@4
+M=D
+
+@4
+D=M
+@LOOP
+D;JGE
+
+@3
+M=-M
+@4
+M=-M
+
+(LOOP)
+@4
+D=M
+@END
+D;JLE
+
+@3
+D=M
+@2
+M=M+D
+
+@4
+M=M-1
+
+@LOOP
+0;JMP
+(END)
+
+@END
+0;JMP
